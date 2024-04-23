ebedebddcd70f13ec35ee6c188bf3a44a7ee0c20
Dym Sohin <re@dym.sh>
2024-04-23 14:31:59 +0200

    2024-apr

---
+++ _installs/socketsupply.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt install -y \
+  build-essential   \
+  clang-14 \
+  git   \
+  libwebkit2gtk-4.1-dev   \
+  libc++abi-14-dev  \
+  libc++-14-dev  \
+  libdbus-1-dev  \
+  pkg-config
+
+
+npm i @socketsupply/socket -g
